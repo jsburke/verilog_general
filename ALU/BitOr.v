85b46fcb87fcf8b2bcc40811aaddd547df4c8946
JSBURKE <jsburke@bu.edu>
2016-10-23 19:53:01 -0400

    bitwise operators complete

--- ALU/BitOr.v
+++ ALU/BitOr.v
@@ -1,4 +1,5 @@
 `timescale 1ns / 1ps
+`include "ALU_inc.v"
 //////////////////////////////////////////////////////////////////////////////////
 //
 //  Bitwise OR
@@ -7,16 +8,16 @@
 module BitOr(operand1, operand2,  //  inputs
 				  result, statusOut);  //  outputs
  
-input [WIDTH-1:0] operand1;
-input [WIDTH-1:0] operand2;
+input [`WIDTH-1:0] operand1;
+input [`WIDTH-1:0] operand2;
 
-output wire [WIDTH-1:0] result;
+output wire [`WIDTH-1:0] result;
 output wire [3:0]       statusOut;
 
 assign result = operand1 | operand2;
-assign statusOut[ST_NEG] = result[WIDTH-1];
-assign statusOut[ST_ZERO] = (result == 0) ? 1 : 0;
-assign statusOut[ST_CARRY] = 0;  //  carry and overflow cannot be set by this
-assign statusOut[ST_OVERFLOW] = 0;
+assign statusOut[`ST_NEG] = result[`WIDTH-1];
+assign statusOut[`ST_ZERO] = (result == 0) ? 1 : 0;
+assign statusOut[`ST_CARRY] = 0;  //  carry and overflow cannot be set by this
+assign statusOut[`ST_OVERFLOW] = 0;
 
 endmodule
