0f0abe01dd36c8555bdbcfd87beae56d7046cd18
JSBURKE <jsburke@bu.edu>
2016-10-23 19:46:20 -0400

    Adder updated with flags

--- ALU/Adder.v
+++ ALU/Adder.v
@@ -1,4 +1,5 @@
 `timescale 1ns / 1ps
+`include "ALU_inc.v"
 //////////////////////////////////////////////////////////////////////////////////
 //
 //  Basic Adder that allows for overflow.  ie FF + 01 = 0 etc
@@ -7,18 +8,18 @@
 module Adder(operand1, operand2,
              result, statusOut);
 				 
-input [WIDTH-1:0] operand1;
-input [WIDTH-1:0] operand2;
+input [`WIDTH-1:0] operand1;
+input [`WIDTH-1:0] operand2;
 
-output [WIDTH-1:0] result;
+output [`WIDTH-1:0] result;
 output [3:0]       statusOut;
 
 wire [1:0] signs;
 
-assign signs = {operand1[WIDTH-1], operand2[WIDTH-1]};
-assign {statusOut[ST_CARRY], result} = operand1 + operand2;
-assign statusOut[ST_NEG] = result[WIDTH-1];
-assign statusOut[ST_ZERO] = (result == 0) ? 1 : 0;
-assign statusOut[ST_OVERFLOW] = (signs[1] != signs[0]) ? 0 : (signs[0] == result[WIDTH-1]) ? 0 : 1;
+assign signs = {operand1[`WIDTH-1], operand2[`WIDTH-1]};
+assign {statusOut[`ST_CARRY], result} = operand1 + operand2;
+assign statusOut[`ST_NEG] = result[`WIDTH-1];
+assign statusOut[`ST_ZERO] = (result == 0) ? 1 : 0;
+assign statusOut[`ST_OVERFLOW] = (signs[1] != signs[0]) ? 0 : (signs[0] == result[`WIDTH-1]) ? 0 : 1;
 
 endmodule
